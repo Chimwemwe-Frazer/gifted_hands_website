00f3eae2c9d1c68004559a244b720a54f60e4b6a
Chimwemwe-Frazer <chimwemwefrazer10@gmail.com>
2026-08-19 22:15:09 +0200

    feat: update background image on guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
 
 </head>
 
-<body class="bg-cover bg-center" style="background-image: url('{{ asset('imgs/optimixed.jpg') }}');">
+<body class="bg-cover bg-center" style="background-image: url('{{ asset('imgs/Medical team home page.jpeg') }}');">
     <div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 mx-6 ">
 
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
